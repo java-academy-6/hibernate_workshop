afcbeef5f23e9b3e2f8cdb8701b44423657a42c0
Dominika Druzgala <dominika_druzgala@epam.com>
2019-02-12 11:50:51 +0100

    Dodano hibernate i h2 oraz konfigurację

---
+++ src/main/java/com/epam/App.java
@@ -0,0 +1,7 @@
+package com.epam;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/epam/Person.java
@@ -0,0 +1,18 @@
+package com.epam;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+}
