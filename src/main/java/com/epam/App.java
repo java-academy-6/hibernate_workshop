7b4c24822da2c7241935d2284b9e2e204334fcec
Mariusz_Krotos <mariusz_krotos@epam.com>
2019-02-20 09:49:35 +0100

    poprawki w zadaniu 7

--- src/main/java/com/epam/App.java
+++ src/main/java/com/epam/App.java
@@ -5,13 +5,18 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) {
         Configuration config = new Configuration().configure("hibernate.cfg.xml");
         SessionFactory sessionFactory = config.buildSessionFactory();
         Session session = sessionFactory.openSession();
-
+        //transient
         Person person = new Person("Kamilek", 26);
+        //persistent
         session.save(person);
 //        person.setName("Buka");
 
@@ -20,16 +25,21 @@ public class App {
         System.out.println();
         System.out.println(load);
         System.out.println();
+        //detached
         session.close();
 
         person.setAge(112);
 
-//        Session nowaSesja = sessionFactory.openSession();
-//
-//        Person nowyKamilek = nowaSesja.load(Person.class, 1);
-//        System.out.println(nowyKamilek);
-//
-//        nowaSesja.close();
+        Session nowaSesja = sessionFactory.openSession();
+        //persistent
+        Person nowyKamilek = nowaSesja.load(Person.class, 1);
+        System.out.println(nowyKamilek);
+
+        nowaSesja.close();
+
+        //removed
+        nowaSesja.delete(person);
+
         sessionFactory.close();
     }
 }
