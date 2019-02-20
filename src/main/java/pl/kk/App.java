0912b25d2db9dc53fdd9759fd7f970bd6fbef4ac
minio111 <admin@programistabezpapierka.pl>
2019-02-20 13:34:39 +0100

    Zadanko dodane

--- src/main/java/pl/kk/App.java
+++ src/main/java/pl/kk/App.java
@@ -3,6 +3,9 @@ package pl.kk;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -12,8 +15,43 @@ public class App {
         Session session = sessionFactory.openSession();
 
         session.beginTransaction();
-        session.save(new User("Dżawa_Trzy_Tysiące"));
+        session.save(new User("Mikołaj"));
+        session.save(new User("Kamil"));
+        session.save(new User("Oskar"));
+        session.save(new User("Oskar"));
+
         session.getTransaction().commit();
+
+        zad1:{
+            Query query = session.createQuery(""); //napisać kwerendę która znajdzie wszystkich użytkowników
+
+            List<User> users = query.getResultList();
+            for (User user : users) {
+                System.out.println(user);
+            }
+            if(users.size()!=3) throw new Error();
+        }
+
+        zad2:{
+            //używająć adnotacji @NamedQuery o nazwie find_user_by_id znaleźć użytkownika o id=2;
+        }
+
+        zad3:{
+            //używająć adnotacji @NamedQuery o nazwie find_all_users znaleźć wszystkich użytkowników;
+        }
+
+        zad4:{
+            //używająć adnotacji @SQLInsert dodać kolejnego użytkownika
+        }
+
+        zad5:{
+            //używająć adnotacji @SQLUpdate zmienić login dodanego wcześniej użytkownika
+        }
+
+        zad6:{
+            //używająć adnotacji @SQLDelete usunąć dodanego wcześniej użytkownika
+        }
+
 
         session.close();
     }
--- src/main/java/pl/kk/User.java
+++ src/main/java/pl/kk/User.java
@@ -1,10 +1,23 @@
 package pl.kk;
 
+import org.hibernate.annotations.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@NamedQuery(name = "find_user_by_id", query = "SELECT id, login FROM user WHERE id = ?")
+@NamedQuery(name = "find_all_users", query = "SELECT id, login FROM user")
+@SQLInsert(sql = "INSERT INTO user (id, login) VALUES (?, ?)",
+        check = ResultCheckStyle.COUNT
+)
+@SQLUpdate(
+        sql = "UPDATE user SET login = ? where id = ?"
+)
+@SQLDelete(
+        sql = "DELETE user WHERE id = ?"
+)
 @Entity
 class User {
 
@@ -32,5 +45,13 @@ class User {
 
     public void setLogin(String login) {
         this.login = login;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                '}';
     }
 }
